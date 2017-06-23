d6a251a30c54469454daa78e5ceab8a874d8f1bf
superphunthyme <igollish@gmail.com>
2017-06-23 13:26:38 -0400

    Method documentation for linked list

--- linked-list.c
+++ linked-list.c
@@ -3,142 +3,144 @@
 #include <stdlib.h>
 #include <string.h>
 
-linked_list* list_create() {
-    linked_list* l = malloc(sizeof(linked_list));
-    check_pointer(l);
-    l->head = NULL;
-    l->size = 0;
-    return l;
+static list_element *init_list_element(T e);
+
+linked_list *list_create() {
+  linked_list *l = malloc(sizeof(linked_list));
+  check_pointer(l);
+  l->head = NULL;
+  l->size = 0;
+  return l;
 }
 
-list_element* init_list_element(T e) {
-    list_element* elem = malloc(sizeof(list_element));
-    check_pointer(elem);
-    elem->next = NULL;
-    // Needs to be default type for T
-    elem->value = e;
-    /* What to do with e -> next? */
-    return elem;
+static list_element *init_list_element(T e) {
+  list_element *elem = malloc(sizeof(list_element));
+  check_pointer(elem);
+  elem->next = NULL;
+  // Needs to be default type for T
+  elem->value = e;
+  /* What to do with e -> next? */
+  return elem;
 }
 
-void list_destroy(linked_list* l) {
-    struct list_element* head = l->head;
-    struct list_element* p;
-    while ((p = head) != NULL) {
-        head = head->next;
-        // Only if value is a pointer
-        free(p->value);
-        free(p);
-    }
-    free(l);
+void list_destroy(linked_list *l) {
+  struct list_element *head = l->head;
+  struct list_element *p;
+  while ((p = head) != NULL) {
+    head = head->next;
+    // Only if value is a pointer
+    free(p->value);
+    free(p);
+  }
+  free(l);
 }
 
 // Add at front for constant time addition
-void list_add(T e, linked_list* l) {
-    list_element* elem = init_list_element(e);
-    struct list_element* n = l->head;
+void list_add(T e, linked_list *l) {
+  list_element *elem = init_list_element(e);
+  struct list_element *n = l->head;
+  l->head = elem;
+  elem->next = n;
+  l->size++;
+}
+
+void list_add_at(int index, T e, linked_list *l) {
+  if (index > l->size || index < 0) {
+    printf("Element must be placed between 0 and the size of the list\n");
+    return;
+  }
+  l->size++;
+  list_element *elem = init_list_element(e);
+  if (index == 0) {
+    struct list_element *n = l->head;
     l->head = elem;
     elem->next = n;
-    l->size++;
+  } else {
+    struct list_element *p = l->head;
+    for (int i = 0; i < index - 1; i++) {
+      p = p->next;
+    }
+    elem->next = p->next;
+    p->next = elem;
+  }
 }
 
-void list_add_at(int index, T e, linked_list* l) {
-    if (index > l->size || index < 0) {
-        printf("Element must be placed between 0 and the size of the list\n");
-        return;
+void list_remove(int index, linked_list *l) {
+  if (index > l->size - 1 || index < 0) {
+    printf("List does not have an element here\n");
+    return;
+  }
+  if (list_is_empty(l)) {
+    printf("Cannot remove from empty list\n");
+    return;
+  }
+  l->size--;
+  if ((index == 0) && (l->head->next == NULL)) {
+    // Only if value is a pointer
+    free(l->head->value);
+    free(l->head);
+  } else if (index == 0) {
+    struct list_element *n = l->head;
+    l->head = l->head->next;
+    // Only if value is a pointer
+    free(n->value);
+    free(n);
+  } else {
+    struct list_element *p = l->head;
+    for (int i = 0; i < index - 1; i++) {
+      p = p->next;
     }
-    l->size++;
-    list_element* elem = init_list_element(e);
-    if (index == 0) {
-        struct list_element* n = l->head;
-        l->head = elem;
-        elem->next = n;
-    } else {
-        struct list_element* p = l->head;
-        for (int i = 0; i < index - 1; i++) {
-            p = p->next;
-        }
-        elem->next = p->next;
-        p->next = elem;
-    }
+    struct list_element *n = p->next;
+    p->next = p->next->next;
+    // Only if value is a pointer
+    free(n->value);
+    free(n);
+  }
 }
 
-void list_remove(int index, linked_list* l) {
-    if (index > l->size - 1 || index < 0) {
-        printf("List does not have an element here\n");
-        return;
-    }
-    if (list_is_empty(l)) {
-        printf("Cannot remove from empty list\n");
-        return;
-    }
-    l->size--;
-    if ((index == 0) && (l->head->next == NULL)) {
-        // Only if value is a pointer
-        free(l->head->value);
-        free(l->head);
-    } else if (index == 0) {
-        struct list_element* n = l->head;
-        l->head = l->head->next;
-        // Only if value is a pointer
-        free(n->value);
-        free(n);
-    } else {
-        struct list_element* p = l->head;
-        for (int i = 0; i < index - 1; i++) {
-            p = p->next;
-        }
-        struct list_element* n = p->next;
-        p->next = p->next->next;
-        // Only if value is a pointer
-        free(n->value);
-        free(n);
-    }
+T list_get(int index, linked_list *l) {
+  if (index > l->size - 1 || index < 0) {
+    printf("List does not have an element here\n");
+  }
+  struct list_element *p = l->head;
+  for (int i = 0; i < index; i++) {
+    p = p->next;
+  }
+  return p->value;
 }
 
-T list_get(int index, linked_list* l) {
-    if (index > l->size - 1 || index < 0) {
-        printf("List does not have an element here\n");
-    }
-    struct list_element* p = l->head;
-    for (int i = 0; i < index; i++) {
-        p = p->next;
-    }
-    return p->value;
+void list_set(int index, T e, linked_list *l) {
+  if (index > l->size - 1 || index < 0) {
+    printf("List does not have an element here\n");
+    return;
+  }
+  if (list_is_empty(l)) {
+    printf("Cannot remove from empty list\n");
+    return;
+  }
+  struct list_element *p = l->head;
+  for (int i = 0; i < index; i++) {
+    p = p->next;
+  }
+  // Will not work for char* unless values are dynamically allocated!!!
+  p->value = e;
 }
 
-void list_set(int index, T e, linked_list* l) {
-        if (index > l->size - 1 || index < 0) {
-            printf("List does not have an element here\n");
-            return;
-        }
-        if (list_is_empty(l)) {
-            printf("Cannot remove from empty list\n");
-            return;
-        }
-        struct list_element* p = l->head;
-        for (int i = 0; i < index; i++) {
-            p = p->next;
-        }
-        // Will not work for char* unless values are dynamically allocated!!!
-        p->value = e;
-}
-
-bool list_contains(T e, linked_list* l) {
-    bool found = false;
-    struct list_element* p = l->head;
-    while (p != NULL) {
-        // This works for for char*, not any type!!!
-        // Assume value cannot be NULL
-        if (equals(e, p->value)) {
-            found = true;
-            break;
-        }
-        p = p->next;
+bool list_contains(T e, linked_list *l) {
+  bool found = false;
+  struct list_element *p = l->head;
+  while (p != NULL) {
+    // This works for for char*, not any type!!!
+    // Assume value cannot be NULL
+    if (equals(e, p->value)) {
+      found = true;
+      break;
     }
-    return found;
+    p = p->next;
+  }
+  return found;
 }
-bool list_is_empty(linked_list* l) {
-    //return (l->head == NULL);
-    return l->size == 0;
+bool list_is_empty(linked_list *l) {
+  //return (l->head == NULL);
+  return l->size == 0;
 }
--- linked-list.h
+++ linked-list.h
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include "board.h"
 
-typedef void* T;
+typedef void *T;
 
 #define check_pointer(p)          \
   if (!p) {                     \
@@ -10,23 +10,125 @@ typedef void* T;
     exit(EXIT_FAILURE);       \
   }
 
+/**
+ * An element of a linked list
+ * */
 typedef struct list_element {
-  struct list_element *next;   /* Pointer to next element in the list */
-  T value;                     /* Value at node */
+  struct list_element *next;   ///< Pointer to next element in the list
+  T value;                     ///< Value at node
 } list_element;
 
+/**
+ * A linked list
+ * */
 typedef struct linked_list {
-  list_element *head;   /* Pointer to the head of the list */
-  int size;       /* Length of list */
+  list_element *head;   ///< Pointer to the head of the list
+  int size;       ///< Length of list
 } linked_list;
 
+/**
+ * @brief Initialize an empty list
+ *
+ * @return Pointer to created list
+ */
 linked_list *list_create();
-list_element *init_list_element();
+
+/**
+ * @brief Destory a a given list
+ *
+ * @param l Pointer to list to be destroyed
+ *
+ * @warning For now, one should remove all the elements, then free the list's
+ * pointer, as the implementation has a bug
+ */
 void list_destroy(linked_list *l);
+
+/**
+ * @brief Add an element with value e to a list
+ *
+ * Add an item at the front of the list, taking constant time.
+ *
+ * @param e The item to add to the list @p l
+ * @param l The list to which @p e is added
+ */
 void list_add(T e, linked_list *l);
+
+/**
+ * @brief Add an element with value e to the list at a specified position
+ *
+ * Add an item at position @p index (list indexing starts at 0), taking linear
+ * time with respect to the index. Will not add the item if the index is out of
+ * bounds.
+ *
+ * @param index The position that the item will occupy in the list
+ * @param e The item to add to the list
+ * @param l The list to which @p e is added
+ */
 void list_add_at(int index, T e, linked_list *l);
+
+/**
+ * @brief Remove an element from a list at a specified position
+ *
+ * Remove an item at position @p index (list indexing starts at 0), taking linear
+ * time with respect to the index.
+ *
+ * @param index The position from which to remove an element
+ * @param l The list from which the element is removed
+ *
+ * @note The item's value is destroyed as well. If additional allocation has
+ * been for the element's value, then this must be manually managed.
+ */
 void list_remove(int index, linked_list *l);
+
+/**
+ * @brief Retrieve a value from a list at a specified position
+ *
+ * Retrieve an value of a list_element at position @p index (list indexing
+ * starts at 0), taking linear time with respect to the index. Will not remove
+ * the item if the index is out of bounds.
+ *
+ * @param index The position from which to find the element
+ * @param l The list from which the element is retrieved
+ */
 T list_get(int index, linked_list *l);
+
+/**
+ * @brief Set a value from a list at a specified position
+ *
+ * Set an value of a list_element at position @p index (list indexing
+ * starts at 0), taking linear time with respect to the index. Will not change
+ * the item if the index is out of bounds.
+ *
+ * @param index The position from which to find the element
+ * @param l The list from which the element is set
+ *
+ * @return The element at the given @p index
+ */
 void list_set(int index, T e, linked_list *l);
+
+/**
+ * @brief Check if a list contains a certain value
+ *
+ * Check that a list contains an element whose value is e.
+ *
+ * @param e The value to find in the list
+ * @param l The list to search
+ *
+ * @return @c TRUE if list contains a list_element with value @p e else @c FALSE
+   list contains a list_element with value @p e*
+ * @warning The implementation must change according to T's type. For the
+ * moment, the equality of board types is declared. The behaviour will be undefined
+ * on other types.
+ */
 bool list_contains(T e, linked_list *l);
+
+/**
+ * @brief Check if a list is empty
+ *
+ * Check that a list is empty (has no list_elements)
+ *
+ * @param l The list to check
+ *
+ * @return @c TRUE if list is empty else @c FALSE
+ */
 bool list_is_empty(linked_list *l);
